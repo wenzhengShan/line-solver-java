96969c95f3a853dc115dc809fbd25225a5d89fcf
Matthew Sheldon <mattsheldon914@gmail.com>
2021-12-02 12:32:48 +0000

    Update GettingStarted.java

--- src/main/java/GettingStarted.java
+++ src/main/java/GettingStarted.java
@@ -13,6 +13,7 @@ public class GettingStarted {
         SolverSSA solverSSA = new SolverSSA();
         solverSSA.compile(model);
         solverSSA.setOptions().samples(100000).seed(50);
+        // Uncomment below to test Tau Leaping
         /*solverSSA.setOptions().configureTauLeap(new TauLeapingType(
                 TauLeapingVarType.Poisson,
                 TauLeapingOrderStrategy.DirectedCycle,
@@ -24,7 +25,8 @@ public class GettingStarted {
     }
 
     public static Network ex1() {
-        long startTime = System.nanoTime();
+        /*  M/M/1 queue
+         */
         Network model = new Network("MM1LowU");
         OpenClass openClass = new OpenClass(model, "Open Class");
         Source source = new Source(model,"Source");
@@ -39,6 +41,8 @@ public class GettingStarted {
     }
 
     public static Network ex2() {
+        /*  M/M/2 queue
+         */
         Network model = new Network("MM2HighU");
         OpenClass openClass = new OpenClass(model, "Open Class");
         Source source = new Source(model,"Source");
@@ -54,6 +58,8 @@ public class GettingStarted {
     }
 
     public static Network ex3() {
+        /*  3 markovian queues in series
+         */
         Network model = new Network("3 Series");
         OpenClass openClass = new OpenClass(model, "Open Class");
         Source source = new Source(model,"Source");
@@ -72,6 +78,8 @@ public class GettingStarted {
     }
 
     public static Network ex4() {
+        /*  3 queues in parallel with Erlang-distributed service times
+         */
         Network model = new Network("Parallel Erlang");
         OpenClass openClass = new OpenClass(model, "Open Class");
         Source source = new Source(model,"Source");
@@ -83,11 +91,25 @@ public class GettingStarted {
         Queue queue3 = new Queue(model, "Queue3", SchedStrategy.FCFS);
         queue3.setService(openClass, new Erlang(16,4));
         Sink sink = new Sink(model, "Sink");
+        
+        
+        RoutingMatrix routingMatrix = new RoutingMatrix(Arrays.asList(openClass),
+                Arrays.asList(source, queue1, queue2, queue3, sink));
+        routingMatrix.addConnection(source, queue1);
+        routingMatrix.addConnection(queue1, sink);
+        routingMatrix.addConnection(source, queue2);
+        routingMatrix.addConnection(queue2, sink);
+        routingMatrix.addConnection(source, queue3);
+        routingMatrix.addConnection(queue3, sink);
+        model.link(routingMatrix);
+
 
         return model;
     }
 
     public static Network ex5() {
+        /* A closed network of 3 queues
+         */
         Network model = new Network("3 Closed");
         Queue queue1 = new Queue(model, "Queue1", SchedStrategy.FCFS);
         Queue queue2 = new Queue(model, "Queue2", SchedStrategy.FCFS);
@@ -105,6 +127,8 @@ public class GettingStarted {
     }
 
     public static Network ex6() {
+        /*An M/M/1/10 queue
+         */
         Network model = new Network("MM1 10");
         OpenClass openClass = new OpenClass(model, "Open Class");
         Source source = new Source(model,"Source");
@@ -120,6 +144,8 @@ public class GettingStarted {
     }
 
     public static Network ex7() {
+        /* A queue with two different open classes
+         */
         Network model = new Network("2CDSDC");
         OpenClass openClass1 = new OpenClass(model, "Open Class 1");
         OpenClass openClass2 = new OpenClass(model, "Open Class 2");
