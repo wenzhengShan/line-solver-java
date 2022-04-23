6081ca18793c201d95b3eeaaf6d963b4243c5fac
Giuliano Casale <g.casale@imperial.ac.uk>
2022-04-23 07:24:58 +0100

    chore: removed loadCopy and saveCopy methods chore: removed project.iml

--- src/main/java/jline/lang/Network.java
+++ src/main/java/jline/lang/Network.java
@@ -364,38 +364,4 @@ public class Network extends Model implements Serializable {
         }
         return acc/accCt;
     }
-
-    public void saveCopy(String fname) {
-        try {
-            FileOutputStream fileOut =
-                    new FileOutputStream("/home/matts/Masters/IndivProj/models/" + fname);
-            ObjectOutputStream out = new ObjectOutputStream(fileOut);
-            out.writeObject(this);
-            out.close();
-            fileOut.close();
-        } catch (IOException i) {
-            i.printStackTrace();
-        }
-    }
-
-
-    public static Network loadCopy(String fname) {
-        Network n = null;
-        try {
-            FileInputStream fileIn = new FileInputStream("/home/matts/Masters/IndivProj/models/" + fname);
-            ObjectInputStream in = new ObjectInputStream(fileIn);
-            n = (Network) in.readObject();
-            in.close();
-            fileIn.close();
-        } catch (IOException i) {
-            i.printStackTrace();
-            return null;
-        } catch (ClassNotFoundException c) {
-            System.out.println("Employee class not found");
-            c.printStackTrace();
-            return null;
-        }
-
-        return n;
-    }
 }
